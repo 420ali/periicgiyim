c5f9535b75dddce32d6662829edecadb3de9f602
Ali Cetin <alicetin2004@icloud.com>
2024-09-30 02:31:15 +0300

    Fixed sum settings problems

--- barkod/settings.py
+++ barkod/settings.py
@@ -117,10 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
   BASE_DIR / 'static'
