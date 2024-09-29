4347cbfbeab456e0e878387f352a79babe1cc7a6
Ali Cetin <alicetin2004@icloud.com>
2024-09-30 02:36:22 +0300

    Settings.py has changed.

--- barkod/settings.py
+++ barkod/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*qbj9@5_w==h^58_20yb%hu6_a&mquecp=qi3=tt6w-pf7kg9o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['periicgiyim.herokuapp.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
